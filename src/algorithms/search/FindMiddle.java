8b1e8989c345056062ae02d22079d8e551b41d89
Jay <24zhangjie@gmail.com>
2017-03-03 23:06:10 +0800

    update

--- src/algorithms/search/FindMiddle.java
+++ src/algorithms/search/FindMiddle.java
@@ -2,5 +2,9 @@ package algorithms.search;
 
 public class FindMiddle
 {
-
+	public static void main(String[] ar) throws InstantiationException, IllegalAccessException
+	{
+		Object o = Object.class.newInstance();
+		System.out.println(o.toString());
+	}
 }
